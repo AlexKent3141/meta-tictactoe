21661272591c723fb34ca34772883e3d5105d86b
Alex Kent <alex.kent1@hotmail.com>
2019-05-23 21:45:10 +0100

    Further tidying of the conditionals using an alias declaration.

--- meta_ttt.h
+++ meta_ttt.h
@@ -63,43 +63,23 @@ struct Node
     static constexpr int Full = p1 != E && p2 != E && p3 != E && p4 != E && p5 != E && p6 != E && p7 != E && p8 != E && p9 != E;
     static constexpr bool GameOver = Full || Ev::Score != D;
 
+    // This alias defines the node resulting from playing a move at the specified piece location.
+    // If the game has already terminated or the move is illegal then a "Result" type will be
+    // returned, otherwise the specified node which will be further expanded.
+    template <Piece pieceAtLocation, class node>
+    using MoveNode = std::conditional_t<GameOver, Result<Ev::Score>,
+                     std::conditional_t<pieceAtLocation == E, node, Illegal>>;
+
     // Search each child node.
-    // Need to check that the game is not already over and that the point is not already occupied.
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p1 == E, Node<p, p2, p3, p4, p5, p6, p7, p8, p9, q>,
-            Illegal>> M1;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p2 == E, Node<p1, p, p3, p4, p5, p6, p7, p8, p9, q>,
-            Illegal>> M2;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p3 == E, Node<p1, p2, p, p4, p5, p6, p7, p8, p9, q>,
-            Illegal>> M3;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p4 == E, Node<p1, p2, p3, p, p5, p6, p7, p8, p9, q>,
-            Illegal>> M4;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p5 == E, Node<p1, p2, p3, p4, p, p6, p7, p8, p9, q>,
-            Illegal>> M5;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p6 == E, Node<p1, p2, p3, p4, p5, p, p7, p8, p9, q>,
-            Illegal>> M6;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p7 == E, Node<p1, p2, p3, p4, p5, p6, p, p8, p9, q>,
-            Illegal>> M7;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p8 == E, Node<p1, p2, p3, p4, p5, p6, p7, p, p9, q>,
-            Illegal>> M8;
-
-    typedef std::conditional_t<GameOver, Result<Ev::Score>,
-            std::conditional_t<p9 == E, Node<p1, p2, p3, p4, p5, p6, p7, p8, p, q>,
-            Illegal>> M9;
+    typedef MoveNode<p1, Node<p, p2, p3, p4, p5, p6, p7, p8, p9, q>> M1;
+    typedef MoveNode<p2, Node<p1, p, p3, p4, p5, p6, p7, p8, p9, q>> M2;
+    typedef MoveNode<p3, Node<p1, p2, p, p4, p5, p6, p7, p8, p9, q>> M3;
+    typedef MoveNode<p4, Node<p1, p2, p3, p, p5, p6, p7, p8, p9, q>> M4;
+    typedef MoveNode<p5, Node<p1, p2, p3, p4, p, p6, p7, p8, p9, q>> M5;
+    typedef MoveNode<p6, Node<p1, p2, p3, p4, p5, p, p7, p8, p9, q>> M6;
+    typedef MoveNode<p7, Node<p1, p2, p3, p4, p5, p6, p, p8, p9, q>> M7;
+    typedef MoveNode<p8, Node<p1, p2, p3, p4, p5, p6, p7, p, p9, q>> M8;
+    typedef MoveNode<p9, Node<p1, p2, p3, p4, p5, p6, p7, p8, p, q>> M9;
 
     static constexpr int ChildVals[] = { M1::Score, M2::Score, M3::Score, M4::Score, M5::Score, M6::Score, M7::Score, M8::Score, M9::Score };
 
